61a01c14b77d93474f1125c95e249fa84e4fd147
Marius <marius.rundereim@gmail.com>
2024-11-14 13:03:19 +0100

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ export default function Home() {
   return (
     <div>
       <h2>Forsiden her!</h2>
+      <h2>Forsiden her!</h2>
+      <h2>Forsiden her!</h2>
+      <h2>Forsiden her!</h2>
+      <h2>Forsiden her!</h2>
     </div>
   );
 }
