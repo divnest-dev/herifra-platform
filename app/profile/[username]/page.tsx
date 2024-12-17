4e8f8da2ec3633fb1b9e3d9cfb684f15bca18d22
Marius <devteam@herifra.no>
2024-12-17 21:07:12 +0100

    test

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,14 +1,10 @@
 "use client";
-
 import { Box, Card, Flex, Text, Button, Avatar } from "@radix-ui/themes";
 import CustomerForm from "./components/CustomerForm";
 import SettingsPanel from "./components/SettingsPanel";
 import { useState } from "react";
-export default function PrivateProfile({
-  params,
-}: {
-  params: { username: string };
-}) {
+
+export default function PrivateProfile() {
   const [activeTab, setActiveTab] = useState("dashboard");
   const profile = {
     name: "Chuck Norris",
@@ -17,86 +13,84 @@ export default function PrivateProfile({
     services: ["Frukt", "Overnatting"],
     image: "/placeholder.jpg",
   };
-  return (
-    <>
-      <div className="w-full">
-        <Flex justify="between" align="center" mb="6">
-          <Text size="8" weight="bold">
-            Din profil
-          </Text>
-          <Button variant="solid" color="gray">
-            Log out
-          </Button>
-        </Flex>
-        <Flex gap="3" mb="6">
-          <Button
-            variant={activeTab === "dashboard" ? "solid" : "soft"}
-            color="grass"
-            onClick={() => setActiveTab("dashboard")}
-          >
-            Legg inn kundeforhold
-          </Button>
-          <Button
-            variant={activeTab === "subscription" ? "solid" : "soft"}
-            color="amber"
-            onClick={() => setActiveTab("subscription")}
-          >
-            Ditt abbonnement
-          </Button>
-          <Button
-            variant={activeTab === "help" ? "solid" : "soft"}
-            color="red"
-            onClick={() => setActiveTab("help")}
-          >
-            Help
-          </Button>
-          <Button
-            variant={activeTab === "settings" ? "solid" : "soft"}
-            color="gray"
-            onClick={() => setActiveTab("settings")}
-          >
-            Innstillinger
-          </Button>
-        </Flex>
-        <Card size="3">
-          <Flex gap="4">
-            <Avatar size="6" src={profile.image} fallback="CN" />
-            <Box>
-              <Text size="6" weight="bold" mb="2">
-                Profildetaljer
-              </Text>
-              <Box>
-                <Flex direction="column">
-                  <Text size="4">{profile.name}</Text>
-                  <Text size="3" color="gray">
-                    {profile.email}
-                  </Text>
-                  <Text size="3" color="gray">
-                    {profile.location}
-                  </Text>
-                </Flex>
-              </Box>
-            </Box>
-          </Flex>
 
-          <Box mt="6">
-            <Text size="5" mb="4">
-              Din profil viser at har tjenester eller at du selger
+  return (
+    <div className="w-full">
+      <Flex justify="between" align="center" mb="6">
+        <Text size="8" weight="bold">
+          Din profil
+        </Text>
+        <Button variant="solid" color="gray">
+          Log out
+        </Button>
+      </Flex>
+      <Flex gap="3" mb="6">
+        <Button
+          variant={activeTab === "dashboard" ? "solid" : "soft"}
+          color="grass"
+          onClick={() => setActiveTab("dashboard")}
+        >
+          Legg inn kundeforhold
+        </Button>
+        <Button
+          variant={activeTab === "subscription" ? "solid" : "soft"}
+          color="amber"
+          onClick={() => setActiveTab("subscription")}
+        >
+          Ditt abbonnement
+        </Button>
+        <Button
+          variant={activeTab === "help" ? "solid" : "soft"}
+          color="red"
+          onClick={() => setActiveTab("help")}
+        >
+          Help
+        </Button>
+        <Button
+          variant={activeTab === "settings" ? "solid" : "soft"}
+          color="gray"
+          onClick={() => setActiveTab("settings")}
+        >
+          Innstillinger
+        </Button>
+      </Flex>
+      <Card size="3">
+        <Flex gap="4">
+          <Avatar size="6" src={profile.image} fallback="CN" />
+          <Box>
+            <Text size="6" weight="bold" mb="2">
+              Profildetaljer
             </Text>
-            <Flex gap="4">
-              {profile.services.map((service) => (
-                <Text key={service}>
-                  {service === "Frukt" ? "🍎" : "🏠"} {service}
+            <Box>
+              <Flex direction="column">
+                <Text size="4">{profile.name}</Text>
+                <Text size="3" color="gray">
+                  {profile.email}
                 </Text>
-              ))}
-            </Flex>
+                <Text size="3" color="gray">
+                  {profile.location}
+                </Text>
+              </Flex>
+            </Box>
           </Box>
-        </Card>
+        </Flex>
         <Box mt="6">
-          {activeTab === "dashboard" && <CustomerForm />}
-          {activeTab === "settings" && <SettingsPanel />}
+          <Text size="5" mb="4">
+            Din profil viser at har tjenester eller at du selger
+          </Text>
+          <Flex gap="4">
+            {profile.services.map((service) => (
+              <Text key={service}>
+                {service === "Frukt" ? "🍎" : "🏠"} {service}
+              </Text>
+            ))}
+          </Flex>
         </Box>
-      </div>
-    </>
+      </Card>
+      <Box mt="6">
+        {activeTab === "dashboard" && <CustomerForm />}
+        {activeTab === "settings" && <SettingsPanel />}
+      </Box>
+    </div>
   );
 }
